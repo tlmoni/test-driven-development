266a90534158ef74656ffd3aa7579cb18cc0e27d
Toni Ojala <toni.tapio.ojala@gmail.com>
2025-02-27 21:35:33 +0200

    Tetris: Pass first test

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -5,9 +5,14 @@ export class Board {
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.createBoard()
+  }
+
+  createBoard() {
+    this.board = Array.from({ length: this.height }, () => ".".repeat(this.width)).join("\n") + "\n";
   }
 
   toString() {
-    return "TODO";
+    return this.board;
   }
 }
