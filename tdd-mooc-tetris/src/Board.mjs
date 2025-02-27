ff5b6c30c2705988902050a0d7559e2767169d1b
Toni Ojala <toni.tapio.ojala@gmail.com>
2025-02-27 21:49:10 +0200

    Tetris: Pass fourth test

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -5,6 +5,8 @@ export class Board {
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.location = 0;
+    this.blockFalling = false;
     this.createBoard();
   }
 
@@ -13,16 +15,22 @@ export class Board {
   }
 
   drop() {
-    const shape = `X`;
+    if (this.blockFalling) {
+      throw "already falling";
+    }
+
+    const shape = "X";
     const index = Math.floor(this.width / 2);
     const updatedBoard = this.board.substring(0, index) + shape + this.board.substring(index + 1);
     this.board = updatedBoard;
     this.location = index;
+    this.blockFalling = true;
   }
 
   tick() {
     const initialLocation = this.location;
     const updatedLocation = this.location + this.width + 1;
+    this.location = updatedLocation;
     const updatedBoard =
       this.board.substring(0, initialLocation) +
       "." +
