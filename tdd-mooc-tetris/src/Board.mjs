ebff93080c78c09f1b11ad9d7ac489ecac56e41a
Toni Ojala <toni.tapio.ojala@gmail.com>
2025-02-27 21:37:15 +0200

    Tetris: Pass second test

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -12,6 +12,14 @@ export class Board {
     this.board = Array.from({ length: this.height }, () => ".".repeat(this.width)).join("\n") + "\n";
   }
 
+  drop() {
+    let shape = `X`;
+    let index = Math.floor(this.width / 2);
+    let updatedBoard = this.board.substring(0, index) + shape + this.board.substring(index + 1);
+    this.board = updatedBoard;
+    this.location = index;
+  }
+
   toString() {
     return this.board;
   }
